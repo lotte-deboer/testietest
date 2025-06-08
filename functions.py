619151d7c58856abf5c0a8abcf4e2c208da53076
Lotte <lottedeboer700@gmail.com>
2025-06-08 14:58:22 +0200

    oefeningen toevoegen

---
+++ functions.py
@@ -0,0 +1,50 @@
+# PI = 3.14159
+#
+# def bereken_inhoud_cilinder(straal=1, hoogte=2):
+#     """Bereken de inhoud van een cilinder"""
+#
+#     inhoud = PI * hoogte * straal ** 2
+#     return(inhoud)
+#
+# result = bereken_inhoud_cilinder()
+# #volledig default
+# print(result)
+# print(bereken_inhoud_cilinder())
+#
+# #1 parameter naam parameter=X
+# print(bereken_inhoud_cilinder(hoogte=10))
+
+#Als je een waarde met value wilt toevoegen, komt die als laatst met aanroepen
+# def bereken_inhoud_blok(lengte, breedte, hoogte):
+#     inhoud = lengte * breedte * hoogte
+#
+#     return (inhoud)
+#
+#
+# bereken_inhoud_blok(1, 2, hoogte=3)
+
+#return(naam functie) is nodig om een waarde erin te krijgen
+
+#print heeft default waarde spatie als seperator en na elke waarde een /n enter
+# met end='XXXX' kan je de enter vervangen door een andere waarde
+# for teller in range(6) :
+#
+#    print(teller, end='; ')
+
+
+
+# Of je kan specifiek de seperator aanspreken en vervangen met een waarde
+print(1, 2, 3, sep=' LOL ', end=' end!')
+
+#elke nieuwe def functie moet een unieke naam hebben, anders foutmelding
+
+
+#local scope, out of scope, global scope
+#globale variabel veranderen in een functie, om de eerder genoemde losse varaibel te overriden
+# global x
+# x = ....
+
+#je kan ook een variable verwijderen
+# del x
+
+
